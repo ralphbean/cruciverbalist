43da01beed2e9688586fd7b87ad494009cbe047f
Ralph Bean <ralph.bean@gmail.com>
2010-11-30 09:34:54 -0500

    formatting

--- cruciverbalist/methods/heuristic.py
+++ cruciverbalist/methods/heuristic.py
@@ -12,43 +12,43 @@ class HeuristicMethod(BaseMethod):
         """ Recursive """
         if len(dct.keys()) == len(working_p):
             return [working_p]
-        else:
-            possibs = []
-            # try placing it on its own across (naive)
-            # try placing it anywhere it will fit across
-            # try placing it anywhere it will fit down
-            
-            # try placing it on its own across (naive)
-            if len(working_p) == 0:
-                ri, ci = [dct.grid_center()]*2
-                entries = [[(ri, ci), 'across'], [(ri, ci), 'down']]
-                for entry in entries:
-                    possibs.extend(
-                        self.good_guess(dct, working_p + [entry], depth+1) )
-                return possibs
-
-            word = dct.keys()[len(working_p)]
-            r = working_p[-1][0][0]
-            c = working_p[-1][0][1]
-            c += (len(dct.keys()[len(working_p)-1]) + 1) * 2
-            entry = [(r, c), 'across']
-            possibs.extend( self.good_guess(dct, working_p + [entry], depth+1) )
-
-            # Look through all the already placed words.
-            for i in range(len(working_p)):
-                # for every placed that `word` fits, try it
-                for j in range(len(dct.keys()[i])):
-                    for k in range(len(word)):
-                        if dct.keys()[i][j] == word[k]:
-                            ri, ci = working_p[i][0]
-                            entry = []
-                            if working_p[i][1] == 'down':
-                                entry = [ (ri + j, ci - k), 'across']
-                            else:
-                                entry = [ (ri - k, ci + j), 'down']
-                            possibs.extend( 
-                                self.good_guess(dct, working_p + [entry], depth+1) )
+        possibs = []
+        # try placing it on its own across (naive)
+        # try placing it anywhere it will fit across
+        # try placing it anywhere it will fit down
+        
+        # try placing it on its own across (naive)
+        if len(working_p) == 0:
+            ri, ci = [dct.grid_center()]*2
+            entries = [[(ri, ci), 'across'], [(ri, ci), 'down']]
+            for entry in entries:
+                possibs.extend(
+                    self.good_guess(dct, working_p + [entry], depth+1) )
             return possibs
+
+        word = dct.keys()[len(working_p)]
+        r = working_p[-1][0][0]
+        c = working_p[-1][0][1]
+        c += (len(dct.keys()[len(working_p)-1]) + 1) * 2
+        entry = [(r, c), 'across']
+        possibs.extend( self.good_guess(dct, working_p + [entry], depth+1) )
+
+        # Look through all the already placed words.
+        for i in range(len(working_p)):
+            # for every placed that `word` fits, try it
+            for j in range(len(dct.keys()[i])):
+                for k in range(len(word)):
+                    if dct.keys()[i][j] == word[k]:
+                        ri, ci = working_p[i][0]
+                        entry = []
+                        if working_p[i][1] == 'down':
+                            entry = [ (ri + j, ci - k), 'across']
+                        else:
+                            entry = [ (ri - k, ci + j), 'down']
+                        possibs.extend( 
+                            self.good_guess(
+                                dct, working_p + [entry], depth+1) )
+        return possibs
 
 
     def produce(self, word_dict):
