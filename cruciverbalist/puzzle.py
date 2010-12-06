58fa87206e6839d65c275e76b65b837cb0dae111
Ralph Bean <ralph.bean@gmail.com>
2010-12-05 22:02:49 -0500

    Removed "rr"

--- cruciverbalist/puzzle.py
+++ cruciverbalist/puzzle.py
@@ -184,7 +184,7 @@ class Puzzle(dict):
 if __name__ == '__main__':
     _words = [
         'dog',
-        'dietpepsi', 'rr', 'ido', 'ti', 'eve', 'step', 'ja', 'janet', 'bigboi',
+        'dietpepsi', 'ido', 'ti', 'eve', 'step', 'ja', 'janet', 'bigboi',
         'serena', 'scoutt', 'toni', 'dr', 'orb']
     for i in range(1, len(_words)):
         words = _words[:i]
